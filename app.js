3040496dc9f27ee80549c92f088b040a125eca1b
Shubham <sanglehsubham9@gmail.com>
2023-03-10 12:14:32 +0530

    deploy

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(express.json());
+app.options('*', cors())
 // app.use(expressFlash)
 app.use(
   expressSession({
